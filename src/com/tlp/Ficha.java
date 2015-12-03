37a3dd7766661fb07e64e8a45be9e9d3d770330f
nontraxx <ignacio.ampuero.13@sansano.usm.cl>
2015-12-03 00:28:06 -0300

    monobranch

--- src/com/tlp/Ficha.java
+++ src/com/tlp/Ficha.java
@@ -3,42 +3,55 @@ package com.tlp;
 import lp.motor.Element;
 
 import java.awt.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Random;
 
 /**
  * Created by nontraxx on 27-11-15.
  */
-public class Ficha {
-    Point pos;
-    int id;
-
-    public Ficha(Point pos, int id)
+public class Ficha implements Element{
+    int posx,posy,jug;
+    boolean dispo=true;
+    Element.Type tip;
+    public Element.Type getType()
     {
-        this.pos = pos;
-        this.id = id;
+        return tip;
+    }
+    public Ficha(int x, int y,Element.Type tipo,int j){
+        this.posx=x;
+        this.posy=y;
+        this.tip=tipo;
+        this.jug=j;
+    }
+    public boolean testAgainst(Element el)
+    {
+        return true;
+    }
+    public void editDistpo(int t, ArrayList<Ficha> fij1,ArrayList<Ficha> fij2){
+        boolean con1=false,cond2=false;
+        //((ff.posx==posx+60 && ff.posy==posy+60) || (ff.posx==posx-60 && ff.posy==posy+60)
     }
 
-    public Point GetPos()
+    public void colocar(Graphics g)
     {
-        return this.pos;
+        if (dispo){
+            g.setColor(Color.YELLOW);
+            g.fillOval(posx,posy,60,60);
+        }
+        if (tip== Element.Type.LEAF){
+            g.setColor(Color.GREEN);
+            g.fillOval(posx+2,posy+2,56,56);
+        }
+        if (tip== Element.Type.WATER){
+            g.setColor(Color.BLUE);
+            g.fillOval(posx+2,posy+2,56,56);
+        }
+        if (tip== Element.Type.FIRE){
+            g.setColor(Color.RED);
+            g.fillOval(posx+2,posy+2,56,56);
+        }
+        g.setColor(Color.BLACK);
+        g.drawString(""+jug+"",posx+30,posy+30);
+        g.drawString("("+posx+","+posy+")",posx+10,posy+50);
+
     }
-
-    public void SetPos(Point pos)
-    {
-        this.pos = pos;
-    }
-
-    public int GetID()
-    {
-        return this.id;
-    }
-
-    public void SetID(int id)
-    {
-        this.id = id;
-    }
-
-
 }
--- src/com/tlp/Main.java
+++ src/com/tlp/Main.java
@@ -1,5 +1,4 @@
-import com.tlp.FichasTipos;
-import com.tlp.FichasPowerUps;
+import com.tlp.Ficha;
 import lp.motor.Application;
 import lp.motor.Context;
 import lp.motor.Element;
@@ -7,16 +6,42 @@ import lp.motor.MouseHandler;
 
 import java.awt.*;
 import java.util.ArrayList;
-
+import java.util.Random;
 
 public class Main implements Context
 {
     private Point pos;
-    private ArrayList<ArrayList<FichasTipos>> fichitas = FichasTipos.HacerListaFichas();
-    private FichasTipos datos = new FichasTipos(pos, Element.Type.values()[0], -1);
-    private FichasTipos aux;
+    private int pnt1=0,pnt2=0,turn=1;
+    private Random rand= new Random();
+    ArrayList<Ficha> fij1=new ArrayList<Ficha>();
+    ArrayList<Ficha> fij2=new ArrayList<Ficha>();
+
     public Main()
     {
+        int i,k;
+
+        ArrayList<Element.Type> elemselect = new ArrayList<Element.Type>();
+        elemselect.add(Element.Type.FIRE);
+        elemselect.add(Element.Type.WATER);
+        elemselect.add(Element.Type.LEAF);
+
+        for (i=0;i<10;i++)
+        {
+
+            k = (i * 120) % 600;
+            if (i<5)
+            {
+                fij1.add(new Ficha(k, 0, elemselect.get(rand.nextInt(3)),1));
+                fij2.add(new Ficha(k, 600-120, elemselect.get(rand.nextInt(3)),2));
+            }
+            else {
+                fij1.add(new Ficha(k+60, 60, elemselect.get(rand.nextInt(3)),1));
+                fij2.add(new Ficha(k+60, 600-60, elemselect.get(rand.nextInt(3)),2));
+            }
+
+        }
+
+        //elemselect.get(rand.nextInt(2));
         // aquí puede inicializar valores y crear los objetos de juego.
     }
 
@@ -26,38 +51,15 @@ public class Main implements Context
         // aquí actualice sus objetos para que puedan interactuar con input de usuario o entre los mismos
         // objetos.
 
+
         // por ejemplo imprimir algo si el mouse está cerca de la esquina superior izquierda:
         pos = mouseHandler.getMousePosition();
-
-        if (mouseHandler.isButtonJustPressed())
-        {
-            aux = FichasTipos.GetFichasTipos(fichitas, pos.x, pos.y);
-            if(!datos.IsPressed()) {
-                if (aux != null)
-                {
-                    aux.Press(true);
-                    datos.CopyFicha(aux);
-                }
-            } else {
-                if (aux == null)
-                {
-                    //System.out.println(aux.GetPos() + " " + datos.GetPos());
-                    if (aux == null){
-                        System.out.println("la wea");
-
-                        FichasTipos.PlaceFicha(fichitas, datos.GetID(), pos);
-                        datos.Press(false);
-                    }
-                }
-
-
-            }
-        }
-
-        //if (pos.x < 200 && pos.y < 200) System.out.println(pos);
+        //if (pos.x < 200 && pos.y < 200)
+        //System.out.println(pos);
     }
     public void tablero(Graphics p)
-    {   boolean black=false;
+    {
+        boolean black=false;
         int x,y;
         for (x=0;x<10;x++)
         {
@@ -70,67 +72,30 @@ public class Main implements Context
                 p.fillRect(x*60,y*60,60,60);
 
             }
+            for(Ficha f: fij1){f.colocar(p);}
+            for(Ficha f: fij2){f.colocar(p);}
         }
     }
-
-    public void DrawFicha(Graphics graphics, FichasTipos fichaIteracion, boolean j1, Point fichaPos)
+    public void texto(int t,int punt1, int punt2,Graphics p)
     {
-        graphics.setColor(fichaIteracion.GetColor());
-        graphics.fillOval(fichaPos.x, fichaPos.y, 30, 30);
-        if (j1)
-        {
-            graphics.setColor(Color.orange);
-        } else {
-            graphics.setColor(Color.LIGHT_GRAY);
-        }
-        graphics.fillOval(fichaPos.x+9, fichaPos.y+9, 12, 12);
+        p.drawString("                    Turno del Jugador "+t+"          Puntaje Player 1 = "+punt1+"          Puntaje Player 2 = "+punt2,0,615);
     }
-
-    public void DibujarFichas(Graphics graphics)
-    {
-        boolean j1 = true;
-        for (ArrayList<FichasTipos> fichaJ : fichitas)
-        {
-            for (FichasTipos fichaIteracion: fichaJ)
-            {
-                if (!fichaIteracion.IsPressed())
-                {
-                    DrawFicha(graphics, fichaIteracion, j1,  fichaIteracion.GetPos());
-                }
-            }
-            j1 = !j1;
-        }
-    }
-
-    public void DibujarFichaMouse(Graphics graphics){
-        if (datos.IsPressed())
-        {
-            if (datos.GetID()%2 == 1){
-                DrawFicha(graphics, datos, false, pos);
-            } else {
-                DrawFicha(graphics, datos, true, pos);
-            }
-        }
-    }
-
     @Override
     public void render(Graphics graphics)
     {
         // aquí, y solo aquí, puede dibujar cosas en la pantalla.
         tablero(graphics);
-        // por ejemplo dibujar un círculo verde:
-        //graphics.setColor(Color.GREEN);
-        //graphics.fillOval(pos.x, pos.y, 30, 30);
+        graphics.setColor(Color.WHITE);
+        texto(turn,pnt1,pnt2,graphics);
+        //graphics.setColor(Color.CYAN);
+        //graphics.fillOval(pos.x-6, pos.y-6, 12, 12);
 
-        DibujarFichas(graphics);
-
-        DibujarFichaMouse(graphics);
 
     }
 
     public static void main(String[] args)
     {
         // el método main solo se encargará de iniciar el sistema.
-        Application.start(600, 600, "Pokemon", 60, new Main());
+        Application.start(600, 620, "Damas_Elementales_Elepé", 60, new Main());
     }
 }
